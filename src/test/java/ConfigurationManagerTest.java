088d699ca03b0dee12910576a9922ecd2de32f4b
Friedrich Ewald <f-ewald@users.github.com>
2016-06-27 11:03:39 -0700

    added unit test for configuration manager

--- src/test/java/ConfigurationManagerTest.java
+++ src/test/java/ConfigurationManagerTest.java
@@ -1,10 +1,13 @@
 import junit.framework.TestCase;
+import net.fewald.jfeedreader.ConfigurationManager;
+import org.junit.Assert;
 
 /**
  * Created by fe on 20.05.16.
  */
 public class ConfigurationManagerTest extends TestCase {
-    public void testSample() {
-        assertTrue(true);
+    public void testConfigurationManager() {
+        ConfigurationManager cm = new ConfigurationManager();
+        Assert.assertNotNull(cm);
     }
 }
